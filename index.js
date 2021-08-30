ab5ba88c90a380941f226c77daeeff277cf2a384
Paul Fell <swellpf@gmail.com>
2021-08-30 13:24:50 -0400

    commit tree 168bd3f6df36918d3e2e0d7116ac1ecfadcc0b84

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 
+function findMinAndRemoveSorted(firstHalf, secondHalf){
+	let minfirstHalf = firstHalf[0]
+	let minsecondHalf = secondHalf[0]
+
+	if(minfirstHalf < minsecondHalf){
+		return firstHalf.shift()
+	} else {
+		return secondHalf.shift()
+	}
+}
+
+function merge(firstHalf, secondHalf){
+	let sorted = []
+	let currentMin;
+	while(firstHalf.length != 0 && secondHalf.length != 0){
+		currentMin = findMinAndRemoveSorted(firstHalf, secondHalf)
+		sorted.push(currentMin)
+	}
+	return sorted.concat(firstHalf).concat(secondHalf)
+}
+
+function mergeSort(array){
+    let midpoint = array.length/2
+    let firstHalf = array.slice(0, midpoint)
+    let secondHalf = array.slice(midpoint, array.length)
+    
+    if(array.length < 2){
+      return array
+    } else {
+      return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+    }
+  }
